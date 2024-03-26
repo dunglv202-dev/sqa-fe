0b5e1433fd1cbcd099c2082bc80098bf7a5e5fbf
dunglv <dunglv202@gmail.com>
2024-03-26 23:50:39 +0700

    re-style loan detail page

--- src/views/list/loans/LoanDetails.js
+++ src/views/list/loans/LoanDetails.js
@@ -4,7 +4,7 @@ import PageContainer from 'src/components/container/PageContainer';
 import { fetchLoan } from 'src/services/listing';
 import DetailField from '../components/DetailField';
 import DetailSection from '../components/DetailSection';
-import { Chip, Typography } from '@mui/material';
+import { Box, Chip, Typography } from '@mui/material';
 import { useTheme } from '@emotion/react';
 
 const LoanType = ({ type }) => {
@@ -38,29 +38,31 @@ const LoanDetails = () => {
 
   return (
     <PageContainer title="Chi tiết khoản vay">
-      <Typography
-        variant="h2"
-        textAlign="center"
-        sx={{ marginBottom: 5, color: theme.palette.primary.main }}
-      >
-        Chi tiết khoản vay
-      </Typography>
-      <DetailSection title="Thông tin khách hàng">
-        <DetailField label="Họ đệm" value={loan?.customer.firstname} />
-        <DetailField label="Tên" value={loan?.customer.lastname} />
-        <DetailField label="CCCD" value={loan?.customer.idCardNo} />
-        <DetailField label="Ngày cấp" value={loan?.customer.idCardIssueDate} />
-        <DetailField label="Nơi cấp" value={loan?.customer.idCardIssueBy} />
-      </DetailSection>
-      <DetailSection title="Thông tin khoản vay">
-        <DetailField label="Số tiền vay" value={loan?.amount} />
-        <DetailField label="Số tiền còn phải trả" value={loan?.remaining} />
-        <DetailField label="Lãi suất (%/năm)" value={loan?.yearlyInterestRate} />
-        <DetailField label="Ngày cho vay" value={loan?.createdAt} />
-        <DetailField label="Ngày đáo hạn" value={loan?.dueDate} />
-        <DetailField label="Mục đích vay" value={loan?.purpose} />
-        <DetailField label="Loại hình vay" value={<LoanType type={loan?.purpose} />} />
-      </DetailSection>
+      <Box sx={{ paddingInline: 10 }}>
+        <Typography
+          variant="h2"
+          textAlign="center"
+          sx={{ marginBottom: 5, color: theme.palette.primary.main }}
+        >
+          Chi tiết khoản vay
+        </Typography>
+        <DetailSection title="Thông tin khách hàng">
+          <DetailField label="Họ đệm" value={loan?.customer.firstname} />
+          <DetailField label="Tên" value={loan?.customer.lastname} />
+          <DetailField label="CCCD" value={loan?.customer.idCardNo} />
+          <DetailField label="Ngày cấp" value={loan?.customer.idCardIssueDate} />
+          <DetailField label="Nơi cấp" value={loan?.customer.idCardIssueBy} />
+        </DetailSection>
+        <DetailSection title="Thông tin khoản vay">
+          <DetailField label="Số tiền vay" value={loan?.amount} />
+          <DetailField label="Số tiền còn phải trả" value={loan?.remaining} />
+          <DetailField label="Lãi suất (%/năm)" value={loan?.yearlyInterestRate} />
+          <DetailField label="Ngày cho vay" value={loan?.createdAt} />
+          <DetailField label="Ngày đáo hạn" value={loan?.dueDate} />
+          <DetailField label="Mục đích vay" value={loan?.purpose} />
+          <DetailField label="Loại hình vay" value={<LoanType type={loan?.purpose} />} />
+        </DetailSection>
+      </Box>
     </PageContainer>
   );
 };
