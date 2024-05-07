b527cbe1399f567843fa543d196725ca0d3ebafe
dunglv <dunglv202@gmail.com>
2024-05-08 00:52:08 +0700

    edit saving report

--- src/views/report/savings/components/Figures.js
+++ src/views/report/savings/components/Figures.js
@@ -12,14 +12,17 @@ const Figures = ({ reportData }) => {
       </Grid>
       <Grid item xs={12} lg={6}>
         <ReportCard
-          figure={reportData.amountPayForDueAccount}
-          desc={'tổng tiền trả cho sổ đáo hạn'}
+          figure={reportData.interestAmountToPay}
+          desc={'tiền lãi phải trả cho khách hàng đang mở sổ'}
         />
       </Grid>
       <Grid item xs={12} lg={6}>
+        <ReportCard figure={reportData.numberOfWithdraw} desc={'sổ tiết kiệm được rút'} />
+      </Grid>
+      <Grid item xs={12} lg={6}>
         <ReportCard
-          figure={reportData.interestPayForDueAccount}
-          desc={'tiền lãi trả cho sổ đáo hạn'}
+          figure={reportData.dueAccountNotWithdraw}
+          desc={'sổ tiết kiệm đã đến hạn chưa rút'}
         />
       </Grid>
     </Grid>
